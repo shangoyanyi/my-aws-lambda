269ab1ef79aba519fffbee8106e71c618513fc51
吳志光 <shangoyanyi@wuzhihikaritekiMacBook-Air.local>
2016-07-25 16:10:08 +0800

    add mod-movies-slackbot.js as a slack request handler

---
+++ mod-movies-slackbot.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var AWS = require('aws-sdk');
+
+var AWSConfig = require('./app/resource/aws-config');
+var db = new AWS.DynamoDB(AWSConfig.getDynamoDBConfig());
+
+/*
+ * handler
+ */
+exports.handler = (event, context, callback) => {
+    var params = {
+        TableName: 'MOD_MOVIES',
+        Limit: 1
+    };
+    
+    db.scan(params, function(err, result){
+        if(err){
+            console.log(err, result);
+            context.fail();
+            
+        }else{
+            console.log(result);
+            console.log(JSON.parse(result.Items[0].data.S));
+            
+            // 建立符合規範的 slack response body
+            var responseBody = {
+                text: "今天的電影清單如下:",
+                attachments: JSON.parse(result.Items[0].data.S)
+            }
+            
+            context.succeed(responseBody);
+        }
+    });
+    
+};
